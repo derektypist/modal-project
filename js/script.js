42576618d2e8cca84a878d09c40dcd83db662005
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2021-04-28 10:06:43 +0000

    Added constants at script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Select modal-btn, modal-overlay and close-btn
+
+const modalBtn = document.querySelector('.modal-btn');
+const modal = document.querySelector('.modal-overlay');
+const closeBtn = document.querySelector('.close-btn');
+
+
