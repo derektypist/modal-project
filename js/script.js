bad1681473c2b539815e3c1fa2aca48158b82e6a
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2021-04-28 10:12:33 +0000

    Updated comment at script.js

--- js/script.js
+++ js/script.js
@@ -4,4 +4,16 @@ const modalBtn = document.querySelector('.modal-btn');
 const modal = document.querySelector('.modal-overlay');
 const closeBtn = document.querySelector('.close-btn');
 
+/* 
+    Listen for click events on modal-btn and close-btn
+    When user clicks modal-btn add .open-modal to modal-overlay
+    When user clicks close-btn remove .open-modal from modal-overlay
+*/
 
+modalBtn = document.addEventListener('click', function() {
+    modal.classList.add('open-modal');
+});
+
+closeBtn = document.addEventListener('click', function() {
+    modal.classList.remove('open-modal');
+});
